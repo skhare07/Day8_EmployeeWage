6aacf7193e6da14a9cb8102a1ac15786e69b750c
Sourav <souravkhare34@gmail.com>
2022-10-28 00:19:11 +0530

    Program to check if employee is Present or Absent

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,7 +1,29 @@
 package com.bridgelabz.day8_EmployeeWage;
 
-public class EmployeeWageComputation {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee wage computation Program");
+import java.util.Random;
+
+
+    class Employee {
+        static final int IS_FULL_TIME = 1;
+
+        public static void empCheck() {
+            Random random = new Random();
+            int empCheck = random.nextInt(2);
+            if (empCheck == IS_FULL_TIME) {
+                System.out.println("PRESENT");
+            } else {
+                System.out.println("ABSENT");
+            }
+        }
     }
-}
+
+    public class EmployeeWageComputation {
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+            Employee.empCheck();
+        }
+
+    }
+
+
