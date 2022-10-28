43f03fa36ae370a5a97e4e581aa39972144fa3a1
Sourav <souravkhare34@gmail.com>
2022-10-28 22:45:03 +0530

    Calculate wage till a condition of total working hours or days is reached for month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,12 +3,12 @@ package com.bridgelabz.day8_EmployeeWage;
 import java.util.Random;
 
 class Employee {
-
     //constant
     static final int IS_FULL_TIME = 1;
     static final int IS_PART_TIME = 2;
     static final int EMP_WAGE_PER_DAY = 20;
     static final int WORKING_DAY_PER_MONTH = 20;
+    static final int TOTAL_WRK_HRS_MONTH = 100;
 
 
     //generating random number between 0 to 2 using random function
@@ -19,7 +19,7 @@ class Employee {
     }
 
     //calculating employee works hour and salary
-    public static int empSalary(int empCheck) {
+    public static int empWrkHrs(int empCheck) {
         int empWrkHrs;
         switch (empCheck) {
 
@@ -27,31 +27,38 @@ class Employee {
             case IS_PART_TIME -> empWrkHrs = 4;
             default -> empWrkHrs = 0;
         }
+            return empWrkHrs;
+    }
 
+    //calculating employee wage per day
+    public static int empWage(int empWrkHrs){
         int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
         return empWage;
-    }
+}
+
 
     //calculating total employee salary
     public static int totalEmpSalary() {
         int day = 0;
         int totalEmpSalary = 0;
-        int empWage;
-        int empCheck;
+        int empWrkHrs;
+        int empWage ;
+        int empCheck ;
+        int totalEmpWrkHrs =0;
 
-        while (day < WORKING_DAY_PER_MONTH) {
+    while ( day < WORKING_DAY_PER_MONTH && totalEmpWrkHrs < TOTAL_WRK_HRS_MONTH ) {
             empCheck = empCheck();
-            empWage = empSalary(empCheck);
+            empWrkHrs = empWrkHrs(empCheck);
+            empWage = empWage(empWrkHrs);
+            totalEmpWrkHrs += empWrkHrs;
             day++;
             totalEmpSalary += empWage;
 
         }
         return totalEmpSalary;
     }
-
 }
 public class EmployeeWageComputation {
-
     public static void main(String[] args) {
 
         int totalEmpSalary;
