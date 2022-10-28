75d6c7e9001ebb5858bf7ea6edcd0966574ff94d
Sourav <souravkhare34@gmail.com>
2022-10-28 19:48:26 +0530

    Program to calculate employee daily wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,23 +5,40 @@ import java.util.Random;
 
     class Employee {
         static final int IS_FULL_TIME = 1;
+        static final int EMP_WAGE_PER_DAY = 20;
 
-        public static void empCheck() {
+        public static int empCheck() {
             Random random = new Random();
             int empCheck = random.nextInt(2);
-            if (empCheck == IS_FULL_TIME) {
-                System.out.println("PRESENT");
-            } else {
-                System.out.println("ABSENT");
-            }
+            return empCheck;
         }
+
+
+    public static void empSalary(int empCheck)
+    {
+        int empWrkHrs = 0;
+        if(empCheck == IS_FULL_TIME){
+            empWrkHrs = 8;
+        }else {
+            empWrkHrs = 0;
+        }
+    int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
+        System.out.println("Employee Wage = "+empWage);
     }
+
+
+    }
+
 
     public class EmployeeWageComputation {
 
         public static void main(String[] args) {
             System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-            Employee.empCheck();
+
+            int empCheck = Employee.empCheck();
+            Employee.empSalary(empCheck);
+
+
         }
 
     }
