c09278c74875596cc829cddcd8332161ce260f34
Sourav <souravkhare34@gmail.com>
2022-10-28 19:59:53 +0530

    Solving employee wage using Switch

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,26 +4,29 @@ import java.util.Random;
 
 
 class Employee {
+
+    //constant
     static final int IS_FULL_TIME = 1;
     static final int IS_PART_TIME = 2;
     static final int EMP_WAGE_PER_DAY = 20;
 
+    //generating random number between 0 to 2 using random function
     public static int empCheck() {
         Random random = new Random();
         int empCheck = random.nextInt(3);
         return empCheck;
     }
 
-
+//calculating employee works hour and salary
     public static void empSalary(int empCheck) {
-        int empWrkHrs = 0;
-        if (empCheck == IS_FULL_TIME) {
-            empWrkHrs = 8;
-        } else if (empCheck == IS_PART_TIME) {
-            empWrkHrs = 4;
-        } else {
-            empWrkHrs = 0;
+        int empWrkHrs;
+        switch (empCheck) {
+
+            case IS_FULL_TIME -> empWrkHrs = 8;
+            case IS_PART_TIME -> empWrkHrs = 4;
+            default -> empWrkHrs = 0;
         }
+
         int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
         System.out.println("Employee Wage = " + empWage);
     }
