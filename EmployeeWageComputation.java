a4818db4d5bd3d78572a762e135c86c803f178e7
Sourav <souravkhare34@gmail.com>
2022-10-28 19:53:33 +0530

    Added part time employee hours

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,44 +3,43 @@ package com.bridgelabz.day8_EmployeeWage;
 import java.util.Random;
 
 
-    class Employee {
-        static final int IS_FULL_TIME = 1;
-        static final int EMP_WAGE_PER_DAY = 20;
+class Employee {
+    static final int IS_FULL_TIME = 1;
+    static final int IS_PART_TIME = 2;
+    static final int EMP_WAGE_PER_DAY = 20;
 
-        public static int empCheck() {
-            Random random = new Random();
-            int empCheck = random.nextInt(2);
-            return empCheck;
-        }
+    public static int empCheck() {
+        Random random = new Random();
+        int empCheck = random.nextInt(3);
+        return empCheck;
+    }
 
 
-    public static void empSalary(int empCheck)
-    {
+    public static void empSalary(int empCheck) {
         int empWrkHrs = 0;
-        if(empCheck == IS_FULL_TIME){
+        if (empCheck == IS_FULL_TIME) {
             empWrkHrs = 8;
-        }else {
+        } else if (empCheck == IS_PART_TIME) {
+            empWrkHrs = 4;
+        } else {
             empWrkHrs = 0;
         }
-    int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
-        System.out.println("Employee Wage = "+empWage);
+        int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
+        System.out.println("Employee Wage = " + empWage);
     }
 
+}
+
+
+public class EmployeeWageComputation {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        int empCheck = Employee.empCheck();
+        Employee.empSalary(empCheck);
 
     }
 
-
-    public class EmployeeWageComputation {
-
-        public static void main(String[] args) {
-            System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-
-            int empCheck = Employee.empCheck();
-            Employee.empSalary(empCheck);
-
-
-        }
-
-    }
+}
 
 
