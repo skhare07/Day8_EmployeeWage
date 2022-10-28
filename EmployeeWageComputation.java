bddd7b07a6656a91ff76b04b5ff9df2cab131b16
Sourav <souravkhare34@gmail.com>
2022-10-28 20:13:21 +0530

    Calculating wages for a Month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,13 +2,14 @@ package com.bridgelabz.day8_EmployeeWage;
 
 import java.util.Random;
 
-
 class Employee {
 
     //constant
     static final int IS_FULL_TIME = 1;
     static final int IS_PART_TIME = 2;
     static final int EMP_WAGE_PER_DAY = 20;
+    static final int WORKING_DAY_PER_MONTH = 20;
+
 
     //generating random number between 0 to 2 using random function
     public static int empCheck() {
@@ -17,8 +18,8 @@ class Employee {
         return empCheck;
     }
 
-//calculating employee works hour and salary
-    public static void empSalary(int empCheck) {
+    //calculating employee works hour and salary
+    public static int empSalary(int empCheck) {
         int empWrkHrs;
         switch (empCheck) {
 
@@ -28,19 +29,34 @@ class Employee {
         }
 
         int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
-        System.out.println("Employee Wage = " + empWage);
+        return empWage;
+    }
+
+    //calculating total employee salary
+    public static int totalEmpSalary() {
+        int day = 0;
+        int totalEmpSalary = 0;
+        int empWage;
+        int empCheck;
+
+        while (day < WORKING_DAY_PER_MONTH) {
+            empCheck = empCheck();
+            empWage = empSalary(empCheck);
+            day++;
+            totalEmpSalary += empWage;
+
+        }
+        return totalEmpSalary;
     }
 
 }
-
-
 public class EmployeeWageComputation {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        int empCheck = Employee.empCheck();
-        Employee.empSalary(empCheck);
 
+        int totalEmpSalary;
+        totalEmpSalary = Employee.totalEmpSalary();
+        System.out.println("Total Employee salary : " + totalEmpSalary);
     }
 
 }
